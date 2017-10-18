163829462f49992e77a586038fb462a048ad1bb1
Michael Vlach <vlach.michael@gmail.com>
2017-10-18 20:33:54 +0200

    Modified the new file so it resolves to different item.

--- examples/ComplexProject/src/libs/ComplexLibrary/include/Include/LibraryInterface.h
+++ examples/ComplexProject/src/libs/ComplexLibrary/include/Include/LibraryInterface.h
@@ -1,15 +1,7 @@
 #pragma once
 
-#include <Common.h>
-
-#ifdef LIBRARY
-#  define LIBRARYSHARED EXPORT
-#else
-#  define LIBRARYSHARED IMPORT
-#endif
-
-class LIBRARYSHARED Library
+class Library
 {
 public:
-    void printMessage(const char *message) const;
+    virtual void printMessage(const char *message) = 0;
 };
